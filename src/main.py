18ae31737397fb7f5fca1f2705a6d1e3e9670745
shintaroDesu <snkraishin87@gmail.com>
2018-11-14 02:56:51 -0500

    add hx711 files

--- src/main.py
+++ src/main.py
@@ -1,46 +1,57 @@
-from twilio.rest import Client
-from email.message import EmailMessage
-import smtplib
+import RPi.GPIO as GPIO
+import time
+import sys
 import os
+from hx711 import HX711
 
+def cleanAndExit():
+    print "Cleaning..."
+    GPIO.cleanup()
+    print "Bye!"
+    sys.exit()
 
-def sendsms(my_string):
-	account_sid = 'AC226ab8ec94356e0e3d7e34614032f855'
-	auth_token = 'ea20f94d90faa85e59378366eb6e218a'
+hx = HX711(5, 6)
 
-	from_number = '+33644603488'
-	to_number = '+33762226688'
-	client = Client(account_sid, auth_token)
+# I've found out that, for some reason, the order of the bytes is not always the same between versions of python, numpy and the hx711 itself.
+# Still need to figure out why does it change.
+# If you're experiencing super random values, change these values to MSB or LSB until to get more stable values.
+# There is some code below to debug and log the order of the bits and the bytes.
+# The first parameter is the order in which the bytes are used to build the "long" value.
+# The second paramter is the order of the bits inside each byte.
+# According to the HX711 Datasheet, the second parameter is MSB so you shouldn't need to modify it.
+hx.set_reading_format("LSB", "MSB")
 
-	client.messages.create(body=my_string, from_=from_number, to=to_number)
+# HOW TO CALCULATE THE REFFERENCE UNIT
+# To set the reference unit to 1. Put 1kg on your sensor or anything you have and know exactly how much it weights.
+# In this case, 92 is 1 gram because, with 1 as a reference unit I got numbers near 0 without any weight
+# and I got numbers around 184000 when I added 2kg. So, according to the rule of thirds:
+# If 2000 grams is 184000 then 1000 grams is 184000 / 2000 = 92.
+#hx.set_reference_unit(113)
+hx.set_reference_unit(92)
 
+hx.reset()
+hx.tare()
 
-def sendmail(subject, my_string):
-	server = smtplib.SMTP('smtp.gmail.com', 587)
-	server.starttls()
+limit = 0 # delta limit barrage pression water
 
-	server.login('snkraishin87@gmail.com', 'encrypted')
-	msg = EmailMessage()
-	msg.set_content(my_string)
+while True:
+    try:
+        # These three lines are usefull to debug wether to use MSB or LSB in the reading formats
+        # for the first parameter of "hx.set_reading_format("LSB", "MSB")".
+        # Comment the two lines "val = hx.get_weight(5)" and "print val" and uncomment the three lines to see what it prints.
+        #np_arr8_string = hx.get_np_arr8_string()
+        #binary_string = hx.get_binary_string()
+        #print binary_string + " " + np_arr8_string
+        
+        # Prints the weight. Comment if you're debbuging the MSB and LSB issue.
+        val = hx.get_weight(5)
+        print val
+        if val >= limit:
+            os.system("python3.5 sendalert.py")
+        
 
-	msg['Subject'] = subject
-	msg['From'] = 'snkraishin87@gmail.com'
-	msg['To'] = 'superhung220703@gmail.com'
-	server.send_message(msg)
-
-
-def check():
-	return True
-
-
-def main():
-	while True:
-		is_dangerous = check() # type : boolean
-
-		if is_dangerous:
-			sendsms("Barrage is dead")
-			sendmail("Barrage", "Barrage is dead")
-
-
-if __name__ == '__main__':
-	main()
+        hx.power_down()
+        hx.power_up()
+        time.sleep(0.5)
+    except (KeyboardInterrupt, SystemExit):
+        cleanAndExit()
---
+++ src/sendalert.py
@@ -0,0 +1,36 @@
+from twilio.rest import Client
+from email.message import EmailMessage
+import smtplib
+import os
+
+
+def sendsms(my_string):
+	account_sid = 'AC226ab8ec94356e0e3d7e34614032f855'
+	auth_token = 'ea20f94d90faa85e59378366eb6e218a'
+
+	from_number = '+33644603488'
+	to_number = '+33762226688'
+	client = Client(account_sid, auth_token)
+
+	client.messages.create(body=my_string, from_=from_number, to=to_number)
+
+
+def sendmail(subject, my_string):
+	server = smtplib.SMTP('smtp.gmail.com', 587)
+	server.starttls()
+
+	server.login('snkraishin87@gmail.com', 'rairyuuaottg')
+	msg = EmailMessage()
+	msg.set_content(my_string)
+
+	msg['Subject'] = subject
+	msg['From'] = 'snkraishin87@gmail.com'
+	msg['To'] = 'superhung220703@gmail.com'
+	server.send_message(msg)
+
+def main():
+    sendsms("Barrage is dead")
+    sendmail("Barrage", "Barrage is dead")
+
+
+main()
