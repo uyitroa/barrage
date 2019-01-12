69fa1f308de33249d6dd7b421bce721f3cf10270
shintaroDesu <snkraishin87@gmail.com>
2019-01-12 05:26:41 -0500

    done

--- src/main.py
+++ src/main.py
@@ -10,7 +10,7 @@ def cleanAndExit():
     print "Bye!"
     sys.exit()
 
-hx = HX711(5, 6)
+hx = HX711(19, 21)
 
 # I've found out that, for some reason, the order of the bytes is not always the same between versions of python, numpy and the hx711 itself.
 # Still need to figure out why does it change.
@@ -19,21 +19,23 @@ hx = HX711(5, 6)
 # The first parameter is the order in which the bytes are used to build the "long" value.
 # The second paramter is the order of the bits inside each byte.
 # According to the HX711 Datasheet, the second parameter is MSB so you shouldn't need to modify it.
-hx.set_reading_format("LSB", "MSB")
+hx.set_reading_format("MSB", "MSB")
 
 # HOW TO CALCULATE THE REFFERENCE UNIT
 # To set the reference unit to 1. Put 1kg on your sensor or anything you have and know exactly how much it weights.
 # In this case, 92 is 1 gram because, with 1 as a reference unit I got numbers near 0 without any weight
 # and I got numbers around 184000 when I added 2kg. So, according to the rule of thirds:
 # If 2000 grams is 184000 then 1000 grams is 184000 / 2000 = 92.
-#hx.set_reference_unit(113)
-hx.set_reference_unit(92)
+#hx.set_reference_unit(1)
 
 hx.reset()
-hx.tare()
+#hx.tare()
 
-limit = 0 # delta limit barrage pression water
-
+# to use both channels, you'll need to tare them both
+#hx.tare_A()
+#hx.tare_B()
+limit = int(sys.argv[1])
+print "Limite:", limit
 while True:
     try:
         # These three lines are usefull to debug wether to use MSB or LSB in the reading formats
@@ -44,14 +46,20 @@ while True:
         #print binary_string + " " + np_arr8_string
         
         # Prints the weight. Comment if you're debbuging the MSB and LSB issue.
-        val = hx.get_weight(5)
+        # val = hx.get_weight(5)
+	val = max(0, int(hx.read_long()))
         print val
-        if val >= limit:
+	if val > limit:
+            print "La force de l'eau depasse la limite. Sending alert..."
             os.system("python3.5 sendalert.py")
-        
+        # To get weight from both channels (if you have load cells hooked up 
+        # to both channel A and B), do something like this
+        #val_A = hx.get_weight_A(5)
+        #val_B = hx.get_weight_B(5)
+        #print "A: %s  B: %s" % ( val_A, val_B )
 
         hx.power_down()
         hx.power_up()
-        time.sleep(0.5)
+        time.sleep(0.1)
     except (KeyboardInterrupt, SystemExit):
         cleanAndExit()
