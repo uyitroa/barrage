8806c7f9169c4a7a53a362c620ce5ad33d24631a
shintaroDesu <snkraishin87@gmail.com>
2019-03-27 13:18:01 -0400

    update discord

--- src/discord_send.py
+++ src/discord_send.py
@@ -1,28 +1,29 @@
 import discord
 import time
+import os
+import asyncio
+
 client = discord.Client()
-TOKEN = 'nah'
+TOKEN = 'lol no'
 
 
 def read_data():
 	return int(open("water_force.txt", "r").read())
 
+async def background_task():
+	await client.wait_until_ready()
+	while not client.is_closed:
+		data = read_data()
+		if data > 50000:
+			user = discord.User(id='276728415518720001')
+			await client.send_message(user, "Alerte: Force de l'eau depasse la limite!")
+			os.system("echo 0 > water_force.txt")
+		time.sleep(0.1)
+		await asyncio.sleep(0.1)
 
-@client.event
-async def on_message(message):
-	data = read_data()
-	print(message.content)
-	if message.content == "status":
-		await client.send_message(message.author, "Force de l'eau actuel: " + str(data))
-	if message.content == "start":
-		while True:
-			data = read_data()
-			if data > 50000:
-				await client.send_message(message.author, "Alerte: Force de l'eau depasse la limite!")
-			time.sleep(0.1)
 
 @client.event
 async def on_ready():
 	print("Starting...")
-
+client.loop.create_task(background_task())
 client.run(TOKEN)
